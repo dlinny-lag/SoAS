b92ad5e1ec3def8c76cb7ea192b35df984209d26
dlinny-lag <dlinny.lag@gmail.com>
2024-03-27 16:57:05 +0500

    updated about info

--- ScenesEditor/AboutDialog.Designer.cs
+++ ScenesEditor/AboutDialog.Designer.cs
@@ -42,7 +42,7 @@ namespace ScenesEditor
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(139, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Animation Scenes. Version: ";
+            this.label1.Text = "Semantics of Animation Scenes Editor. Version: ";
             // 
             // versionLabel
             // 
